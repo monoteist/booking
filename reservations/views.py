7fd62eb6d8e81911e48c13805018aaaf0d9aeed6
monoteist <i.ezerbaev97@gmail.com>
2024-05-04 07:18:58 +0300

    add viewsets for reservations

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Property, Booking
+from .serializers import PropertySerializer, BookingSerializer
+
+
+class PropertyViewSet(viewsets.ModelViewSet):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.user_type == 'tenant':
+            return Booking.objects.filter(tenant=user)
+        elif user.user_type == 'owner':
+            return Booking.objects.filter(property__owner=user)
+        return super().get_queryset()
