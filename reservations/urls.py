2cdca7753441ca89d07877d83fa6728fbcd7f7e1
monoteist <i.ezerbaev97@gmail.com>
2024-05-04 07:20:35 +0300

    add routers for reservation views

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,8 +1,11 @@
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
+from .views import PropertyViewSet, BookingViewSet
+
 router_v1 = DefaultRouter()
+router_v1.register('properties', PropertyViewSet)
+router_v1.register('bookings', BookingViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
